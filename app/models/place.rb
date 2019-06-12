24c07c6c6c66fa516d6ed97cd1dc6380c2123d18
Ashley <ashjohnson318@gmail.com>
2019-06-12 17:28:09 +0000

    Nomster lesson 28

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,8 @@
 class Place < ApplicationRecord
-  belongs_to :user
-  validates :name, presence: true
+  belongs_to :user 
+  validates :name, length: { minimum: 3, 
+  	too_short: "%{count} characters is the minimum amount allowed"}
+  validates :address, :description,  presence: true
 
   self.per_page = 3
 end
