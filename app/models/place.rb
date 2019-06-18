617109f89f9e40be47f28a37d6dcc192eef705f2
Ashley <ashjohnson318@gmail.com>
2019-06-18 15:43:21 +0000

    Nomster 39.1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,13 @@
 class Place < ApplicationRecord
   belongs_to :user 
   has_many :comments
+  has_many :photos
+  
   geocoded_by :address
+  
   after_validation :geocode
-  validates :name, length: { minimum: 3, 
+  
+  validates :name, presence: true, length: { minimum: 3, 
   	too_short: "%{count} characters is the minimum amount allowed"}
   validates :address, :description,  presence: true
 
